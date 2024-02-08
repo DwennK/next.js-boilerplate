ba70738f4fc209154342320383b3770d60a68e6b
DwennK <dwenn@live.fr>
2024-02-09 00:32:50 +0100

    modify header

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,9 +2,20 @@ import Logo from "@/components/Logo";
 
 function Header() {
   return (
-    <div>
-      <Logo />
-    </div>
+    <header className="sticky top-0 -z50 bg-white dark:bg-gray-900">
+      <nav className="flex flex-col sm:flex-row items-center p-5 pl-2 bg-white dark:bg-gray-900 max-w-7xl mx-auto">
+        <Logo />
+        <div className="flex-1 flex items-center justify-end space-x-4">
+          {/* Language Select*/}
+
+          {/* Session && */}
+
+          {/* DarkmodeToggle*/}
+
+          {/* UserButton */}
+        </div>
+      </nav>
+    </header>
   );
 }
 
