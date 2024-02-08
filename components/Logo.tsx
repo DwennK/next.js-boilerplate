a61f12deae0b0e13e9e09b4ddfec9928b7e8a288
DwennK <dwenn@live.fr>
2024-02-09 00:21:00 +0100

    Add Logo.tsx

---
+++ components/Logo.tsx
@@ -0,0 +1,25 @@
+import LogoImage from "@/assets/images/logo.svg";
+import Link from "next/link";
+import Image from "next/image";
+import { AspectRatio } from "@/components/ui/aspect-ratio";
+
+function Logo() {
+  return (
+    <Link href="/" prefetch={false} className="overflow-hidden">
+      <div className="flex items-center w-72 h-14">
+        <AspectRatio
+          ratio={16 / 9}
+          className="flex items-center justify-center"
+        >
+          <Image
+            priority
+            src={LogoImage}
+            alt="logo"
+            className="dark:filter dark:invert"
+          />
+        </AspectRatio>
+      </div>
+    </Link>
+  );
+}
+export default LogoImage;
