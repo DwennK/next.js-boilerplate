f2d7e299ab87a4ff21d483c9f2d33697a1e9c2ea
DwennK <dwenn@live.fr>
2024-02-13 00:46:09 +0100

    Add Pricing link if session doesnt exist. (Pricing page not done yet)

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,6 +3,8 @@ import DarkModeToggle from "@/components/DarkModeToggle";
 import UserButton from "@/components/UserButton";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/auth";
+import Link from "next/link";
+import { MessagesSquareIcon } from "lucide-react";
 
 async function Header() {
   const session = await getServerSession(authOptions);
@@ -13,7 +15,18 @@ async function Header() {
         <div className="flex-1 flex items-center justify-end space-x-4">
           {/* Language Select*/}
 
-          {/* Session && */}
+          {/* If session exists (), else ()*/}
+          {session ? (
+            <>
+              <Link href={"/chat"} prefetch={false}>
+                <MessagesSquareIcon className="text-black dark:text-white" />
+              </Link>
+            </>
+          ) : (
+            <Link href={"/pricing"} className="text-black dark:text-white">
+              Pricing
+            </Link>
+          )}
 
           <DarkModeToggle />
 
