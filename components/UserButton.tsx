895d35a9a9c1d241a0771741e5140b18f3efe681
DwennK <dwenn@live.fr>
2024-02-13 01:16:33 +0100

    - Add array for authorized domains for images in next.config.js - Add image from google to UserButton

--- components/UserButton.tsx
+++ components/UserButton.tsx
@@ -21,22 +21,24 @@ function UserButton({ session }: { session: Session | null }) {
       </Button>
     );
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger>
-        <UserAvatar
-          name="Dwenn Kaufmann"
-          image="https://github.com/shadcn.png"
-        ></UserAvatar>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent>
-        <DropdownMenuLabel>My Account</DropdownMenuLabel>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem>Profile</DropdownMenuItem>
-        <DropdownMenuItem>Billing</DropdownMenuItem>
-        <DropdownMenuItem>Team</DropdownMenuItem>
-        <DropdownMenuItem>Subscription</DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    session && (
+      <DropdownMenu>
+        <DropdownMenuTrigger>
+          <UserAvatar
+            name={session.user?.name}
+            image={session.user?.image}
+          ></UserAvatar>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>Profile</DropdownMenuItem>
+          <DropdownMenuItem>Billing</DropdownMenuItem>
+          <DropdownMenuItem>Team</DropdownMenuItem>
+          <DropdownMenuItem>Subscription</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    )
   );
 }
 
--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,14 @@
+/* Array of authorized domains for remote images */
+const hostnames = ["github.com", "lh3.googleusercontent.com"];
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* Nous sert à définir les hostname authorisés pour les images */
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "github.com",
-        port: "",
-        pathname: "/**",
-      },
-    ],
+    remotePatterns: hostnames.map((hostname) => ({
+      protocol: "https",
+      hostname,
+    })),
   },
 };
 
